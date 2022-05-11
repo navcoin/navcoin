4308c86ee3d24927d1f7267d944e95b4ee546e01
mxaddict <mxaddict@codedmaster.com>
2022-05-11 23:08:03 +0800

    Added new src/blsct/bulletproofs.* files

---
+++ src/blsct/bulletproofs.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 The Navcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+// Diverse arithmetic operations in the bls curve
+// inspired by https://github.com/b-g-goodell/research-lab/blob/master/source-code/StringCT-java/src/how/monero/hodl/bulletproof/Bulletproof.java
+// and https://github.com/monero-project/monero/blob/master/src/ringct/bulletproofs.cc
+
+#include <blsct/bulletproofs.h>
