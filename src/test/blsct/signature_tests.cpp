62dafb958fb14d2b6ce7be0fdc08011fc072d5a4
gogoex <110195520+gogoex@users.noreply.github.com>
2023-01-16 10:05:29 +0900

    add skeleton code

---
+++ src/test/blsct/signature_tests.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 The Navcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+//
+#include <test/util/setup_common.h>
+
+#include <blsct/signature.h>
+
+BOOST_FIXTURE_TEST_SUITE(signature_tests, MclTestingSetup)
+
+BOOST_AUTO_TEST_CASE(test_foo)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
