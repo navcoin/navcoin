7bcbd5d167361c772dc19515478d77eef9f3f0f4
gogoex <110195520+gogoex@users.noreply.github.com>
2023-05-14 10:02:35 +0900

    clean up

--- src/test/blsct/set_mem_proof/set_mem_proof_prover_tests.cpp
+++ src/test/blsct/set_mem_proof/set_mem_proof_prover_tests.cpp
@@ -9,7 +9,6 @@
 #include <test/util/setup_common.h>
 #include <cstdio>
 #include <sstream>
-#include <blsct/arith/mcl/mcl.h>
 #include <blsct/arith/elements.h>
 #include <blsct/building_block/pedersen_commitment.h>
 #include <blsct/set_mem_proof/set_mem_proof_prover.h>
@@ -18,7 +17,6 @@
 
 BOOST_FIXTURE_TEST_SUITE(set_mem_proof_prover_tests, BasicTestingSetup)
 
-//using Arith = Mcl;
 using Arith = Secp256k1;
 using Point = Arith::Point;
 using Scalar = Arith::Scalar;
