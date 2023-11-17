a5c66b1a2796a99c4e4a91e1a5fa6cd50a68a65b
gogoex <110195520+gogoex@users.noreply.github.com>
2023-11-17 09:16:52 +0900

    wip

--- src/test/bech32_mod_tests.cpp
+++ src/test/bech32_mod_tests.cpp
@@ -119,6 +119,11 @@ BOOST_AUTO_TEST_CASE(bech32_mod_test_detecting_errors)
     }
 }
 
+BOOST_AUTO_TEST_CASE(bech32_mod_test_vec_recovery)
+{
+    // TODO write this test
+}
+
 BOOST_AUTO_TEST_CASE(bech32_mod_test_vec8_to_vec5)
 {
     {
@@ -270,150 +275,5 @@ BOOST_AUTO_TEST_CASE(bech32_mod_test_vec5_to_vec8)
         BOOST_CHECK(data_v8[4] == 0b1001'1110);
     }
 }
-/*
-BOOST_AUTO_TEST_CASE(bech32_mod_testvectors_valid)
-{
-    static const std::string CASES[] = {
-        "A12UEL5L",
-        "a12uel5l",
-        "an83characterlonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1tt5tgs",
-        "abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw",
-        "11qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc8247j",
-        "split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w",
-        "?1ezyfcl",
-    };
-    for (const std::string& str : CASES) {
-        const auto dec = bech32_mod::Decode(str);
-        BOOST_CHECK(dec.encoding == bech32_mod::Encoding::BECH32);
-        std::string recode = bech32_mod::Encode(bech32_mod::Encoding::BECH32, dec.hrp, dec.data);
-        BOOST_CHECK(!recode.empty());
-        BOOST_CHECK(CaseInsensitiveEqual(str, recode));
-    }
-}
-
-BOOST_AUTO_TEST_CASE(bech32m_mod_testvectors_valid)
-{
-    static const std::string CASES[] = {
-        "A1LQFN3A",
-        "a1lqfn3a",
-        "an83characterlonghumanreadablepartthatcontainsthetheexcludedcharactersbioandnumber11sg7hg6",
-        "abcdef1l7aum6echk45nj3s0wdvt2fg8x9yrzpqzd3ryx",
-        "11llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllludsr8",
-        "split1checkupstagehandshakeupstreamerranterredcaperredlc445v",
-        "?1v759aa"
-    };
-    for (const std::string& str : CASES) {
-        const auto dec = bech32_mod::Decode(str);
-        BOOST_CHECK(dec.encoding == bech32_mod::Encoding::BECH32M);
-        std::string recode = bech32_mod::Encode(bech32_mod::Encoding::BECH32M, dec.hrp, dec.data);
-        BOOST_CHECK(!recode.empty());
-        BOOST_CHECK(CaseInsensitiveEqual(str, recode));
-    }
-}
-
-BOOST_AUTO_TEST_CASE(bech32_mod_testvectors_invalid)
-{
-    static const std::string CASES[] = {
-        " 1nwldj5",
-        "\x7f""1axkwrx",
-        "\x80""1eym55h",
-        "an84characterslonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1569pvx",
-        "pzry9x0s0muk",
-        "1pzry9x0s0muk",
-        "x1b4n0q5v",
-        "li1dgmt3",
-        "de1lg7wt\xff",
-        "A1G7SGD8",
-        "10a06t8",
-        "1qzzfhee",
-        "a12UEL5L",
-        "A12uEL5L",
-        "abcdef1qpzrz9x8gf2tvdw0s3jn54khce6mua7lmqqqxw",
-        "test1zg69w7y6hn0aqy352euf40x77qddq3dc",
-    };
-    static const std::pair<std::string, std::vector<int>> ERRORS[] = {
-        {"Invalid character or mixed case", {0}},
-        {"Invalid character or mixed case", {0}},
-        {"Invalid character or mixed case", {0}},
-        {"Bech32 string too long", {90}},
-        {"Missing separator", {}},
-        {"Invalid separator position", {0}},
-        {"Invalid Base 32 character", {2}},
-        {"Invalid separator position", {2}},
-        {"Invalid character or mixed case", {8}},
-        {"Invalid checksum", {}}, // The checksum is calculated using the uppercase form so the entire string is invalid, not just a few characters
-        {"Invalid separator position", {0}},
-        {"Invalid separator position", {0}},
-        {"Invalid character or mixed case", {3, 4, 5, 7}},
-        {"Invalid character or mixed case", {3}},
-        {"Invalid Bech32 checksum", {11}},
-        {"Invalid Bech32 checksum", {9, 16}},
-    };
-    static_assert(std::size(CASES) == std::size(ERRORS), "Bech32 CASES and ERRORS should have the same length");
-
-    int i = 0;
-    for (const std::string& str : CASES) {
-        const auto& err = ERRORS[i];
-        const auto dec = bech32_mod::Decode(str);
-        BOOST_CHECK(dec.encoding == bech32_mod::Encoding::INVALID);
-        auto [error, error_locations] = bech32_mod::LocateErrors(str);
-        BOOST_CHECK_EQUAL(err.first, error);
-        BOOST_CHECK(err.second == error_locations);
-        i++;
-    }
-}
-
-BOOST_AUTO_TEST_CASE(bech32m_mod_testvectors_invalid)
-{
-    static const std::string CASES[] = {
-        " 1xj0phk",
-        "\x7f""1g6xzxy",
-        "\x80""1vctc34",
-        "an84characterslonghumanreadablepartthatcontainsthetheexcludedcharactersbioandnumber11d6pts4",
-        "qyrz8wqd2c9m",
-        "1qyrz8wqd2c9m",
-        "y1b0jsk6g",
-        "lt1igcx5c0",
-        "in1muywd",
-        "mm1crxm3i",
-        "au1s5cgom",
-        "M1VUXWEZ",
-        "16plkw9",
-        "1p2gdwpf",
-        "abcdef1l7aum6echk45nj2s0wdvt2fg8x9yrzpqzd3ryx",
-        "test1zg69v7y60n00qy352euf40x77qcusag6",
-    };
-    static const std::pair<std::string, std::vector<int>> ERRORS[] = {
-        {"Invalid character or mixed case", {0}},
-        {"Invalid character or mixed case", {0}},
-        {"Invalid character or mixed case", {0}},
-        {"Bech32 string too long", {90}},
-        {"Missing separator", {}},
-        {"Invalid separator position", {0}},
-        {"Invalid Base 32 character", {2}},
-        {"Invalid Base 32 character", {3}},
-        {"Invalid separator position", {2}},
-        {"Invalid Base 32 character", {8}},
-        {"Invalid Base 32 character", {7}},
-        {"Invalid checksum", {}},
-        {"Invalid separator position", {0}},
-        {"Invalid separator position", {0}},
-        {"Invalid Bech32m checksum", {21}},
-        {"Invalid Bech32m checksum", {13, 32}},
-    };
-    static_assert(std::size(CASES) == std::size(ERRORS), "Bech32m CASES and ERRORS should have the same length");
-
-    int i = 0;
-    for (const std::string& str : CASES) {
-        const auto& err = ERRORS[i];
-        const auto dec = bech32_mod::Decode(str);
-        BOOST_CHECK(dec.encoding == bech32_mod::Encoding::INVALID);
-        auto [error, error_locations] = bech32_mod::LocateErrors(str);
-        BOOST_CHECK_EQUAL(err.first, error);
-        BOOST_CHECK(err.second == error_locations);
-        i++;
-    }
-}
-*/
 
 BOOST_AUTO_TEST_SUITE_END()
