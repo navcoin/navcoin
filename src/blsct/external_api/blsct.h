c8e0c348ff0b4ad0c649f6156809a01a73022335
gogoex <110195520+gogoex@users.noreply.github.com>
2024-01-20 06:11:27 +0900

    use locally defined constants

--- src/blsct/external_api/blsct.h
+++ src/blsct/external_api/blsct.h
@@ -17,8 +17,8 @@ enum Chain {
 };
 
 struct DoublePublicKey {
-    uint8_t vk[blsct::PublicKey::SIZE];
-    uint8_t sk[blsct::PublicKey::SIZE];
+    uint8_t vk[PUBLIC_KEY_SIZE];
+    uint8_t sk[PUBLIC_KEY_SIZE];
 };
 
 bool blsct_init(Chain chain);
@@ -27,13 +27,13 @@ bool blsct_init(Chain chain);
 // string of size DOUBLE_PUBKEY_ENC_SIZE
 bool decode_blsct_address(
     const char* blsct_addr,
-    uint8_t dpk[blsct::DoublePublicKey::SIZE]
+    uint8_t dpk[DOUBLE_PUBLIC_KEY_SIZE]
 );
 
 // blsct_addr is expected to be a null-terminated c-style
 // string of size DOUBLE_PUBKEY_ENC_SIZE
 bool encode_blsct_address(
-    const uint8_t dpk[blsct::DoublePublicKey::SIZE],
+    const uint8_t dpk[DOUBLE_PUBKEY_ENC_SIZE],
     const char* blsct_addr
 );
 
