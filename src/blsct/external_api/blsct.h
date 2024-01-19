9d1339430fe0b67b47933eabf5e0501a4c032dce
gogoex <110195520+gogoex@users.noreply.github.com>
2024-01-20 07:17:28 +0900

    support c in blsct.h

--- src/blsct/external_api/blsct.h
+++ src/blsct/external_api/blsct.h
@@ -4,10 +4,14 @@
 #define DOUBLE_PUBLIC_KEY_SIZE 96
 #define ENCODED_DPK_SIZE 165
 
-// API designed for JavaScript, Python, C, Rust, and Golang
-// with primary focus on JavaScript, Python, and C
+/*
+ * API designed for JavaScript, Python, C, Rust, and Golang
+ * with primary focus on JavaScript, Python, and C
+ */
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 
 enum Chain {
     MainNet,
@@ -26,35 +30,43 @@ enum AddressEncoding {
 
 bool blsct_init(Chain chain);
 
-// blsct_addr: a null-terminated c-style string of length ENCODED_DPK_SIZE
-// ser_dpk: a 48-byte vk followed by a 48-byte sk
+/*
+ * blsct_addr: a null-terminated c-style string of length ENCODED_DPK_SIZE
+ * ser_dpk: a 48-byte vk followed by a 48-byte sk
+ */
 bool blsct_decode_address(
     const char* blsct_addr,
     uint8_t ser_dpk[ENCODED_DPK_SIZE]
 );
 
-// ser_dpk: a 48-byte vk followed by a 48-byte sk
-// blsct_addr: a buffer of size at least ENCODED_DPK_SIZE + 1
+/*
+ * ser_dpk: a 48-byte vk followed by a 48-byte sk
+ * blsct_addr: a buffer of size at least ENCODED_DPK_SIZE + 1
+ */
 bool blsct_encode_address(
     const uint8_t ser_dpk[ENCODED_DPK_SIZE],
     char* blsct_addr,
     AddressEncoding encoding
 );
 
-// - range proof creation and verification
-// BuildRangeProof
-// VerifyRangeProof
-//
-// - blsct signatures creation/verification
-// Sign
-// VerifySignature
-//
-// - rangeproof amount/memo recovery
-// RecoverAmount
-// RecoverMemo
-//
-// - transaction serialization/deserialization
-// SerializeTransaction
-// DeserializeTransaction
+/*
+- range proof creation and verification
+BuildRangeProof
+VerifyRangeProof
 
+- blsct signatures creation/verification
+Sign
+VerifySignature
+
+- rangeproof amount/memo recovery
+RecoverAmount
+RecoverMemo
+
+- transaction serialization/deserialization
+SerializeTransaction
+DeserializeTransaction
+*/
+
+#ifdef __cplusplus
 } // extern "C"
+#endif
