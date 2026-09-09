f9ba4dd8c024c8f697ec7f7d945ebc9f2813eda8
Fabian Jahr <fjahr@protonmail.com>
2026-09-09 09:45:19 +0200

    fix(net): treat RFC 9637 IPv6 documentation range as invalid
    Backport of https://github.com/bitcoin/bitcoin/pull/36199 (upstream commit e6c775c6d63a).
    
    (cherry picked from commit a0ae200d99ceef24e7cc85112272532ba692678b)

--- src/netaddress.cpp
+++ src/netaddress.cpp
@@ -344,6 +344,12 @@ bool CNetAddr::IsRFC3849() const
     return IsIPv6() && HasPrefix(m_addr, std::array<uint8_t, 4>{0x20, 0x01, 0x0D, 0xB8});
 }
 
+bool CNetAddr::IsRFC9637() const
+{
+    return IsIPv6() && HasPrefix(m_addr, std::array<uint8_t, 2>{0x3F, 0xFF}) &&
+           (m_addr[2] & 0xF0) == 0x00;
+}
+
 bool CNetAddr::IsRFC3964() const
 {
     return IsIPv6() && HasPrefix(m_addr, std::array<uint8_t, 2>{0x20, 0x02});
@@ -435,7 +441,7 @@ bool CNetAddr::IsValid() const
     }
 
     // documentation IPv6 address
-    if (IsRFC3849())
+    if (IsRFC3849() || IsRFC9637())
         return false;
 
     if (IsInternal())
